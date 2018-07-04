6a9646690c5273835687c193086e2ba734d4cec3
Filip Hoško <filip.hosko@younics.com>
2018-07-04 12:27:19 +0200

    Add super.componentDidMount call

--- src/EditControl.js
+++ src/EditControl.js
@@ -1,23 +1,22 @@
-import { PropTypes } from "prop-types";
-import Draw from "leaflet-draw"; // eslint-disable-line
-import isEqual from "lodash.isequal";
+import { PropTypes } from 'prop-types';
+import Draw from 'leaflet-draw'; // eslint-disable-line
+import isEqual from 'lodash.isequal';
 
-import { LayersControl } from "react-leaflet";
-import { Map } from "leaflet";
+import { LayersControl } from 'react-leaflet';
+import { Map } from 'leaflet';
 
 const eventHandlers = {
-  onEdited: "draw:edited",
-  onDrawStart: "draw:drawstart",
-  onDrawStop: "draw:drawstop",
-  onDrawVertex: "draw:drawvertex",
-  onEditStart: "draw:editstart",
-  onEditMove: "draw:editmove",
-  onEditResize: "draw:editresize",
-  onEditVertex: "draw:editvertex",
-  onEditStop: "draw:editstop",
-  // onDeleted: "draw:deleted",
-  onDeleteStart: "draw:deletestart",
-  onDeleteStop: "draw:deletestop"
+  onEdited: 'draw:edited',
+  onDrawStart: 'draw:drawstart',
+  onDrawStop: 'draw:drawstop',
+  onDrawVertex: 'draw:drawvertex',
+  onEditStart: 'draw:editstart',
+  onEditMove: 'draw:editmove',
+  onEditResize: 'draw:editresize',
+  onEditVertex: 'draw:editvertex',
+  onEditStop: 'draw:editstop',
+  onDeleteStart: 'draw:deletestart',
+  onDeleteStop: 'draw:deletestop'
 };
 
 export default class EditControl extends LayersControl {
@@ -43,10 +42,10 @@ export default class EditControl extends LayersControl {
       allowIntersection: PropTypes.bool
     }),
     position: PropTypes.oneOf([
-      "topright",
-      "topleft",
-      "bottomright",
-      "bottomleft"
+      'topright',
+      'topleft',
+      'bottomright',
+      'bottomleft'
     ])
   };
 
@@ -63,13 +62,13 @@ export default class EditControl extends LayersControl {
     const { layerContainer } = this.context;
 
     layerContainer.addLayer(e.layer);
-    if (typeof onCreated === "function") onCreated(e);
+    if (typeof onCreated === 'function') onCreated(e);
   };
 
   onDeleted = e => {
     const { onDeleted } = this.props;
 
-    if (typeof onDeleted === "function") onDeleted(e);
+    if (typeof onDeleted === 'function') onDeleted(e);
   };
 
   componentWillMount() {
@@ -77,8 +76,8 @@ export default class EditControl extends LayersControl {
 
     this.updateDrawControls();
 
-    map.on("draw:created", this.onDrawCreate);
-    map.on("draw:deleted", this.onDeleted);
+    map.on('draw:created', this.onDrawCreate);
+    map.on('draw:deleted', this.onDeleted);
 
     for (const key in eventHandlers) {
       if (this.props[key]) {
@@ -90,15 +89,16 @@ export default class EditControl extends LayersControl {
   componentDidMount() {
     const { onMounted } = this.props;
 
-    if (typeof onMounted === "function") onMounted(this.leafletElement);
+    super.componentDidMount();
+    if (typeof onMounted === 'function') onMounted(this.leafletElement);
   }
 
   componentWillUnmount() {
     const { map } = this.context;
     this.leafletElement.remove(map);
 
-    map.off("draw:created", this.onDrawCreate);
-    map.off("draw:deleted", this.onDeleted);
+    map.off('draw:created', this.onDrawCreate);
+    map.off('draw:deleted', this.onDeleted);
 
     for (const key in eventHandlers) {
       if (this.props[key]) {
